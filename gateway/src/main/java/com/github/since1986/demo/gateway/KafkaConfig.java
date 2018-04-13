f78f4448bd68bdd1bd54343547473c9cf6bdbbf3
since1986 <385741668@qq.com>
2018-04-13 13:35:16 +0800

    注册补全了信息

--- gateway/src/main/java/com/github/since1986/demo/gateway/KafkaConfig.java
+++ gateway/src/main/java/com/github/since1986/demo/gateway/KafkaConfig.java
@@ -1,9 +1,43 @@
 package com.github.since1986.demo.gateway;
 
+import com.github.since1986.demo.gateway.model.RemoteCallEvent;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @EnableKafka
 @Configuration
 public class KafkaConfig {
+
+    //    @Value("${kafka.bootstrap-servers}")
+    private String bootstrapServers = "localhost:9092";
+
+    @Bean
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return props;
+    }
+
+    @Bean
+    public ProducerFactory<String, RemoteCallEvent> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    public KafkaTemplate<String, RemoteCallEvent> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
 }
--- profile/src/main/java/com/github/since1986/demo/profile/KafkaConfig.java
+++ profile/src/main/java/com/github/since1986/demo/profile/KafkaConfig.java
@@ -1,9 +1,48 @@
 package com.github.since1986.demo.profile;
 
+import com.github.since1986.demo.profile.model.RemoteCallEvent;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @EnableKafka
 @Configuration
 public class KafkaConfig {
+
+    //    @Value("${kafka.bootstrap-servers}")
+    private String bootstrapServers = "localhost:9092";
+
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "demo-group");
+        return props;
+    }
+
+    @Bean
+    public ConsumerFactory<String, RemoteCallEvent> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs(), new StringDeserializer(),
+                new JsonDeserializer<>(RemoteCallEvent.class));
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, RemoteCallEvent> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, RemoteCallEvent> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+
+        return factory;
+    }
 }
